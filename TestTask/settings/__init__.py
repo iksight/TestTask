47dcd30d240c6a598f14f496db7b52ce7bc5f5e5
iksight <iksight@outlook.com>
2018-05-22 23:29:46 +0300

    Create .gitignore, configure.

---
+++ TestTask/settings/__init__.py
@@ -0,0 +1,12 @@
+from split_settings.tools import include
+
+# split-settings
+
+include(
+    'apps.py',
+    'auth.py',
+    'common.py',
+    'database.py',
+    'allauth.py',
+    'adminlte2.py'
+)
---
+++ TestTask/settings/common.py
@@ -0,0 +1,29 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+DEBUG = os.getenv('DEBUG', True)
+
+ALLOWED_HOSTS = []
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 't)2z-(h0pw+pok2f9#vn$h0$v8od)0n-3)hxpnw!kf$#zfuxum'
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_URL = '/static/'
